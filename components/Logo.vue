df223109db74612a4014aaccc4081d65ae342ce2
christopher <christopher@datamaran.com>
2024-09-21 18:35:16 +0200

    add logo placeholder

---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="rounded-full bg-white px-4 py-2.5">e</div>
+</template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-    class="bg-zinc-900 sticky h-20 top-10 rounded-2xl flex items-center space-between p-4"
+    class="bg-zinc-900 sticky h-20 top-10 rounded-2xl flex items-center justify-between p-4"
   >
+    <Logo />
     <XButton>Create new trip</XButton>
   </div>
 </template>
