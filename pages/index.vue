dca18fac59b3f3bf0fa110dabe3c6d76d4016c57
christopher <christopher@datamaran.com>
2024-09-22 19:15:57 +0200

    remove initial view (unrequired layer)

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <script setup>
-import InitialView from "~/components/InitialView.vue";
+import useData from "~/composables/useData";
+import Card from "~/components/Card.vue";
+import TripButtonGroup from "~/components/Trip/ButtonGroup.vue";
+
+const { filteredTrips } = useData();
 </script>
 
 <template>
-  <InitialView />
+  <div class="initial-view flex flex-col gap-6" v-if="filteredTrips">
+    <TripButtonGroup />
+
+    <Card
+      v-for="{ id, title, description, photo_url } in filteredTrips"
+      :key="id"
+      :trip="{ title, description, photo_url }"
+    />
+  </div>
 </template>
