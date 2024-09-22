529f0cfa8e3e3d637868ba2370667059bbb111f9
christopher <christopher@datamaran.com>
2024-09-22 14:13:30 +0200

    add card cmp

---
+++ components/Card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface BaseTrip {
+  id: number;
+  title: string;
+  photo_url: string;
+  description: string;
+}
+
+defineProps<{
+  trip: BaseTrip;
+}>();
+</script>
+
+<template>card</template>
