870da5ebda44449e7552e49083edae1b5e1ea682
christopher <christopher@datamaran.com>
2024-09-22 14:13:55 +0200

    add initial view

--- components/InitialView.vue
+++ components/InitialView.vue
@@ -1,9 +1,16 @@
 <script setup>
 import useData from "~/composables/useData";
+import Card from "./Card.vue";
 
-const { data } = useData();
+const { data: trips } = await useData();
 </script>
 
 <template>
-  <div class="initial-view">{{ data[0] }}</div>
+  <div class="initial-view">
+    <Card
+      v-for="{ id, title, description, photo_url } in trips"
+      :key="id"
+      :trip="{ id, title, description, photo_url }"
+    />
+  </div>
 </template>
