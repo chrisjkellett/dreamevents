3608fe5336984e8a93c1b4b6a1c7fa75bd9fec87
christopher <christopher@datamaran.com>
2024-09-22 13:23:26 +0200

    move endpoint mock inside describe

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,12 +2,12 @@ import { mountSuspended, registerEndpoint } from "@nuxt/test-utils/runtime";
 import { it, describe, expect } from "vitest";
 import app from "~/app.vue";
 
-registerEndpoint(
-  "https://my-json-server.typicode.com/mariosanz92/dream-travels-data/travels",
-  () => ["foobar"]
-);
-
 describe("First test", () => {
+  registerEndpoint(
+    "https://my-json-server.typicode.com/mariosanz92/dream-travels-data/travels",
+    () => ["foobar"]
+  );
+
   it("should render", async () => {
     const wrapper = await mountSuspended(app);
     console.log(wrapper.find(".initial-view").html());
