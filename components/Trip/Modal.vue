1598b2aa3b5d024d477a126714bdab3d8ae9fee8
christopher <christopher@datamaran.com>
2024-09-23 19:28:28 +0200

    basic bulleted list for itinerary

--- components/Trip/Modal.vue
+++ components/Trip/Modal.vue
@@ -38,6 +38,14 @@ function onClose() {
         <div>
           <h3 class="text-xl">Itinerary</h3>
         </div>
+        <ol class="pl-4 relative border-s border-slate-600 ml-4">
+          <li v-for="item in trip.itinerary" class="mb-10 ms-4 py-2">
+            <div
+              class="absolute w-3 h-3 bg-slate-600 rounded-full mt-1.5 -start-1.5 border border-white"
+            ></div>
+            {{ item }}
+          </li>
+        </ol>
       </div>
     </div>
   </UModal>
