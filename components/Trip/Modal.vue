8a9ce1508b949975256a0964af2c5a1e929aba54
christopher <christopher@datamaran.com>
2024-09-23 20:42:08 +0200

    better icon

--- components/Trip/Modal.vue
+++ components/Trip/Modal.vue
@@ -22,6 +22,9 @@ function onClose() {
     }"
   >
     <div class="flex flex-col min-h-screen relative">
+      <div
+        class="w-7 h-7 absolute top-4 right-4 rounded-full bg-slate-800"
+      ></div>
       <UIcon
         name="i-heroicons:x-circle"
         class="w-7 h-7 absolute top-4 right-4 text-white cursor-pointer"
