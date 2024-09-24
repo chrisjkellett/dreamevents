c195e46a07c55e5df47b0bb5acd5e7a4f70ca8e5
christopher <christopher@datamaran.com>
2024-09-25 00:23:02 +0100

    updates status

--- components/Trip/UpdateStatus.vue
+++ components/Trip/UpdateStatus.vue
@@ -2,14 +2,19 @@
 import useData from "~/composables/useData";
 
 const { currentlyViewedTrip, updateStatusFromViewed } = useData();
+
+const isTodo = computed(() => currentlyViewedTrip.value.status === "todo");
 </script>
 
 <template>
-  <div
-    @click="updateStatusFromViewed"
-    class="cursor-pointer hover:bg-green-200 w-fit"
-  >
-    update status
+  <div @click="updateStatusFromViewed" class="cursor-pointer">
+    <div v-if="isTodo" class="text-gray-500 flex items-center gap-1">
+      <UIcon name="i-heroicons:check-circle" class="w-5 h-5" />
+      <span>Mark as completed</span>
+    </div>
+    <div v-else class="text-green-500 flex items-center gap-1">
+      <UIcon name="i-heroicons:check-circle-solid" class="w-5 h-5" />
+      <span>Complete</span>
+    </div>
   </div>
-  <div>{{ currentlyViewedTrip.status }}</div>
 </template>
