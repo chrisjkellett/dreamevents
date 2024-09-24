7623d822f36960971897b875383a78e370cd567c
christopher <christopher@datamaran.com>
2024-09-25 00:32:59 +0100

    update styles

--- components/Trip/UpdateStatus.vue
+++ components/Trip/UpdateStatus.vue
@@ -3,18 +3,27 @@ import useData from "~/composables/useData";
 
 const { currentlyViewedTrip, updateStatusFromViewed } = useData();
 
-const isTodo = computed(() => currentlyViewedTrip.value.status === "todo");
+const styles = computed(() => {
+  const todo = currentlyViewedTrip.value.status === "todo";
+  return {
+    iconName: todo
+      ? "i-heroicons:check-circle"
+      : "i-heroicons:check-circle-solid",
+    color: todo ? "text-gray-500" : "text-green-500",
+    text: todo ? "Mark as completed" : "Complete",
+  };
+});
 </script>
 
 <template>
-  <div @click="updateStatusFromViewed" class="cursor-pointer">
-    <div v-if="isTodo" class="text-gray-500 flex items-center gap-1">
-      <UIcon name="i-heroicons:check-circle" class="w-5 h-5" />
-      <span>Mark as completed</span>
-    </div>
-    <div v-else class="text-green-500 flex items-center gap-1">
-      <UIcon name="i-heroicons:check-circle-solid" class="w-5 h-5" />
-      <span>Complete</span>
-    </div>
+  <div
+    @click="updateStatusFromViewed"
+    class="cursor-pointer flex items-center gap-1"
+    :class="styles.color"
+  >
+    <UIcon :name="styles.iconName" class="w-5 h-5" />
+    <span>
+      {{ styles.text }}
+    </span>
   </div>
 </template>
