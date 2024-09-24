c2107f81533a076292e1e9c7dd4cc09d9a27744b
christopher <christopher@datamaran.com>
2024-09-24 21:56:37 +0200

    select menu

---
+++ components/X/SelectMenu.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{ modelValue: string; options: string[] }>();
+
+defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+</script>
+
+<template>
+  <USelectMenu
+    :model-value="modelValue"
+    @update:model-value="$emit('update:modelValue', $event)"
+    size="lg"
+    class="focus:ring-blue-500"
+    placeholder="Select a status"
+    :options="options"
+    :ui="{
+      rounded: 'rounded-full',
+      color: {
+        gray: {
+          outline: 'focus:ring-gray-300',
+        },
+      },
+    }"
+  />
+</template>
