d6159579178df37c299f23d39f76517723065413
christopher <christopher@datamaran.com>
2024-09-24 21:48:25 +0200

    text area cmp

---
+++ components/X/TextArea.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{ modelValue: string }>();
+
+defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+</script>
+
+<template>
+  <UTextarea
+    :model-value="modelValue"
+    @update:model-value="$emit('update:modelValue', $event)"
+    size="lg"
+    class="focus:ring-blue-500"
+    color="gray"
+    :ui="{
+      rounded: 'rounded-3xl',
+      color: {
+        gray: {
+          outline: 'focus:ring-gray-300',
+        },
+      },
+    }"
+  />
+</template>
