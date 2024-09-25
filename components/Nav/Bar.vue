7ca1a940499d18cf9acfa3bf642530061c59cef1
christopher <christopher@datamaran.com>
2024-09-25 01:53:09 +0100

    remove logo

--- components/Nav/Bar.vue
+++ components/Nav/Bar.vue
@@ -1,13 +1,11 @@
 <script setup>
-import Logo from "~/components/Nav/Logo.vue";
 import XButton from "~/components/X/Button.vue";
 </script>
 
 <template>
   <div
-    class="bg-zinc-900 sticky h-20 md:rounded-2xl flex items-center justify-between p-4"
+    class="bg-zinc-900 sticky h-20 md:rounded-2xl flex items-center justify-end p-4"
   >
-    <Logo />
     <XButton color="white" disabled>Create new trip</XButton>
   </div>
 </template>
